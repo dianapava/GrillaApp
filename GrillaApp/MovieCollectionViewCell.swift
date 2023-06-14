05ed203b4dce55af4868ab0e8e2c7af8da78a13f
diana pava <dianapava65@gmail.com>
2023-06-13 20:43:49 -0500

    inicio de proyecto

---
+++ GrillaApp/MovieCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieCollectionViewCell.swift
+//  GrillaApp
+//
+//  Created by Diana Pava Avila on 13/06/23.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleSection: UILabel!
+    @IBOutlet weak var collectionSection: UICollectionView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
